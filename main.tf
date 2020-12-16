86dcd1849908e471c54f141780b24f9150317c40
Loïc Stevens <stevensloic@gmail.com>
2020-12-16 23:37:12 +0100

    fix: instance size

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "mongodbatlas_cluster" "my_awesome_cluster" {
     project_id = var.project_id
     name = var.cluster_name
     provider_name = "GCP"
-    provider_instance_size_name = "M0"
+    provider_instance_size_name = "M0 Sandbox"
+    provider_region_name    = "europe-west-1"
     mongo_db_major_version="4.2"
 }
